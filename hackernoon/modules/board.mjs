950e3cacfa96a2a948137b1a2e0221a54fde59ac
Vladimir Shnayder <volodyaschneider@icloud.com>
2023-05-28 19:40:06 +0300

    hackernoon version

---
+++ hackernoon/modules/board.mjs
@@ -0,0 +1,35 @@
+import {
+    DEFAULT_CELL_SIZE,
+    BOARD_BACKGROUND_COLOR
+} from './constants.mjs'
+
+export class Board {
+    #cellSize = DEFAULT_CELL_SIZE
+    #backgroundColor = BOARD_BACKGROUND_COLOR
+
+    constructor(canvas) {
+        this.canvas = canvas
+        this.ctx = this.canvas.getContext("2d")
+    }
+
+    drawBackground() {
+        const { width, height } = this.canvas
+
+        this.ctx.fillStyle = this.#backgroundColor
+        this.ctx.fillRect(0, 0, width, height)
+    }
+
+    get size() {
+        const { width, height } = this.canvas
+
+        return {
+            cellNumberX: Math.ceil(width / this.#cellSize),
+            cellNumberY: Math.ceil(height / this.#cellSize),
+            cellSize: this.#cellSize,
+        }
+    }
+
+    get context() {
+        return this.ctx
+    }
+}
---
+++ hackernoon/modules/constants.mjs
@@ -0,0 +1,7 @@
+export const DEFAULT_CELL_SIZE = 10
+
+export const BOARD_WIDTH = document.documentElement.offsetWidth
+export const BOARD_HEIGHT = document.documentElement.offsetHeight
+
+export const DEFAULT_CELL_COLOR = "rgb(0, 255, 0)"
+export const BOARD_BACKGROUND_COLOR = "rgb(0, 0, 0)"
